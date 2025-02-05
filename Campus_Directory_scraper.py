fb2eab5255b3f640d9e62378bb2062723b0e93df
Manish Kolla <maneeshkolla@gmail.com>
2025-02-05 09:15:49 -0500

    Updated a python scraper code

--- Campus_Directory_scraper.py
+++ Campus_Directory_scraper.py
@@ -1,3 +1,4 @@
+#Importing libraries for scraper, please ensure you have the respective drivers installed and path
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
